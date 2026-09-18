97e5ed54ae8fb2784cac2c9e195fbf622365e717
christian-gomezp <christian.gomezp@udea.edu.co>
2026-09-17 19:27:54 -0500

    Add fromAccountId, toAccountId, and amount
    Added account details for transactions.

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,4 +4,7 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "john"
     val password = "demo"
+    val fromAccountId = "54321"
+    val toAccountId = "13011"
+    val amount = "1"
 }
